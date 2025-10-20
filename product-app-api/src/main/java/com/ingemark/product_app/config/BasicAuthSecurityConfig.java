5b596773f01595f48bae5321937327fda5f9699f
joskozoric <liukang3@gmail.com>
2025-10-20 10:12:41 +0200

    Revamped security config  - separate conditional configurations for oauth and basic auth

--- product-app-api/src/main/java/com/ingemark/product_app/config/BasicAuthSecurityConfig.java
+++ product-app-api/src/main/java/com/ingemark/product_app/config/BasicAuthSecurityConfig.java
@@ -3,20 +3,23 @@ package com.ingemark.product_app.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
-
+@Profile("local-no-docker")
+public class BasicAuthSecurityConfig {
     @Bean
-    @Profile("local-no-docker")
     public SecurityFilterChain basicAuthFilterCHain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
@@ -30,20 +33,18 @@ public class SecurityConfig {
     }
 
     @Bean
-    @Profile("local-with-docker")
-    public SecurityFilterChain oauthFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/","/swagger-ui/**", "/v3/api-docs/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/products/**").hasAuthority("SCOPE_product_app_api:product.read")
-                        .requestMatchers(HttpMethod.POST, "/products").hasAuthority("SCOPE_product_app_api:product.write")
-                        .anyRequest().authenticated()
-                )
-                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable);;
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
-        return http.build();
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder.encode("password"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
     }
 }
---
+++ product-app-api/src/main/java/com/ingemark/product_app/config/OAuthSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.ingemark.product_app.config;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.*;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@ConditionalOnProperty("spring.security.oauth2.resourceserver.jwt.issuer-uri")
+public class OAuthSecurityConfig {
+    @Bean
+    public SecurityFilterChain oauthFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/","/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/products/**").hasAuthority("SCOPE_product_app_api:product.read")
+                        .requestMatchers(HttpMethod.POST, "/products").hasAuthority("SCOPE_product_app_api:product.write")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable);;
+
+        return http.build();
+    }
+}
