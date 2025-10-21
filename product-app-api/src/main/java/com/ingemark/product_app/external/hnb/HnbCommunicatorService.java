e2b85b8d4fb49d34d5d00d30b78c18b6691d8f66
joskozoric <liukang3@gmail.com>
2025-10-21 05:53:30 +0200

    Refactored HnbCommunicatorService  - uses ConcurrentHashMap  - verify application date of newly fetched rates

--- product-app-api/src/main/java/com/ingemark/product_app/external/hnb/HnbCommunicatorService.java
+++ product-app-api/src/main/java/com/ingemark/product_app/external/hnb/HnbCommunicatorService.java
@@ -4,20 +4,24 @@ import com.ingemark.product_app.external.hnb.dto.HnbExchangeRateDto;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Service
 public class HnbCommunicatorService {
 
     private static final Logger log = LoggerFactory.getLogger(HnbCommunicatorService.class);
 
-    private final List<HnbExchangeRateDto> currentRates = new CopyOnWriteArrayList<>();
+    private final Map<String, HnbExchangeRateDto> currentRates = new ConcurrentHashMap<>(20, 0.75f, 1); // only the refresh task can write
 
     private final WebClient webClient;
 
@@ -36,17 +40,35 @@ public class HnbCommunicatorService {
                     .bodyToMono(new ParameterizedTypeReference<List<HnbExchangeRateDto>>() {})
                     .block();
 
-            if (newRates != null && !newRates.isEmpty()) {
-                currentRates.clear();
-                currentRates.addAll(newRates);
-                log.info("Successfully fetched and stored {} exchange rates. Effective date: {}",
-                        newRates.size(), newRates.get(0).getApplicationDate());
-            } else {
+            if (CollectionUtils.isEmpty(newRates)) {
                 log.warn("HNB returned an empty list of exchange rates");
+                return;
             }
+
+            newRates.forEach(this::insertOrUpdateExistingRate);
+            log.info("Successfully fetched and stored {} exchange rates.",  newRates.size());
         } catch (Exception e) {
             log.error("Error fetching exchange rates from HNB: {}", e.getMessage(), e);
         }
+    }
+
+    void insertOrUpdateExistingRate(HnbExchangeRateDto newRate) {
+        currentRates.merge(newRate.getCurrency(), newRate, (existingValue, newValue) -> {
+            try {
+                // compare application dates
+                LocalDate newRateDate = LocalDate.parse(newValue.getApplicationDate(),
+                        DateTimeFormatter.ISO_LOCAL_DATE);
+                LocalDate currentRateDate = LocalDate.parse(existingValue.getApplicationDate(),
+                        DateTimeFormatter.ISO_LOCAL_DATE);
+
+                if(newRateDate.isAfter(currentRateDate)){
+                    return newValue;
+                }
+            } catch (Exception e) {
+                log.warn("Exception while parsing application date for: {}, skipping...", newValue.getCurrency());
+            }
+            return existingValue;
+        });
     }
 
     public BigDecimal getMiddleRateUSD() {
@@ -54,9 +76,6 @@ public class HnbCommunicatorService {
     }
 
     public BigDecimal getMiddleRateForCurrency(String currency) {
-            return currentRates.stream()
-                    .filter(dto -> dto.getCurrency() != null && dto.getCurrency().equalsIgnoreCase(currency))
-                    .map(HnbExchangeRateDto::getMiddleRate)
-                    .findFirst().orElse(BigDecimal.ZERO);
+            return currentRates.containsKey(currency) ? currentRates.get(currency).getMiddleRate() : BigDecimal.ZERO;
     }
 }
