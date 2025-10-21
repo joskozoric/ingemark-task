51762acd6f71bfea806025714b33f4737f373c3b
joskozoric <liukang3@gmail.com>
2025-10-21 20:42:10 +0200

    Security config tweak for swagger-ui

--- product-app-api/src/main/java/com/ingemark/product_app/config/OAuthSecurityConfig.java
+++ product-app-api/src/main/java/com/ingemark/product_app/config/OAuthSecurityConfig.java
@@ -18,7 +18,7 @@ public class OAuthSecurityConfig {
     public SecurityFilterChain oauthFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/","/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .requestMatchers("/","/swagger-ui/**", "/swagger-ui.html", "/v3/api-docs/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/products/**").hasAuthority("SCOPE_product_app_api:product.read")
                         .requestMatchers(HttpMethod.POST, "/products").hasAuthority("SCOPE_product_app_api:product.write")
                         .anyRequest().authenticated()
@@ -26,7 +26,7 @@ public class OAuthSecurityConfig {
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
                 .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable);;
+                .cors(AbstractHttpConfigurer::disable);
 
         return http.build();
     }
